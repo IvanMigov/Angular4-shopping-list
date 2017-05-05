17b6599293e71fbd8dfa0cb0473bd5b6711ec97c
ivan <ivan.migov@gmail.com>
2017-05-05 22:28:54 +0300

    lesson 110

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -9,8 +9,12 @@ export class ShoppingListService {
   getIngredients(){
     return this.ingredients.slice();
   }
-  addIngredients(ingredient:Ingredient){
+  addIngredient(ingredient:Ingredient){
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+  addIngredients(ingredients:Ingredient[]){
+    this.ingredients.push(...ingredients);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
