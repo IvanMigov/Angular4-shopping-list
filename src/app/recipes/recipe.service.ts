0373605fa26cbe434950f15c1789868f69e9bf17
ivan <ivan.migov@gmail.com>
2017-05-17 00:39:50 +0300

    lecture 220

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,8 +2,10 @@ import { Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService {
+  recipeChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -29,14 +31,32 @@ export class RecipeService {
   constructor(private slService:ShoppingListService){
 
   }
-  getRecipes(){
+
+  getRecipes() {
     return this.recipes.slice();
   }
-  getRecipe(index:number){
+
+  getRecipe(index: number) {
     return this.recipes[index];
   }
-  addIngredientsToShoppingList(ingredients:Ingredient[]){
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients)
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+
+  }
+
+  updateRecipe(index: number, recipe: Recipe) {
+    this.recipes[index] = recipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index,1);
+    this.recipeChanged.next(this.recipes.slice());
   }
 
 }
