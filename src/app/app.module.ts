ffd181eae10ac8af749b9522d9b46eb8b04f4225
Ivan Migov <ivan@revimedia.com>
2017-05-23 19:58:38 +0300

    ds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,23 @@ import {RecipeService} from "./recipes/recipe.service";
 import {ServerService} from "./servers.service";
 import {AuthService} from "./auth/auth.service";
 import {AuthGuard} from "./auth/auth-guard.service";
-import {RecipesModule} from "./recipes/recipes.module";
+// import {RecipesModule} from "./recipes/recipes.module";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    RecipesModule,
+    // RecipesModule,
     SharedModule,
     ShoppingListModule,
     AuthModule
